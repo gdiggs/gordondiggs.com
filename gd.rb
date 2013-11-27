e1a462a6860382ec607dd7d844d2ee3519b8294e
GordonDiggs <gordon@gordondiggs.com>
2013-11-27 16:03:13 -0500

    Get tweets

--- gd.rb
+++ gd.rb
@@ -4,6 +4,16 @@ Bundler.require
 DEFAULT_NUM_ITEMS = 10
 CATEGORIES = ['tweets', 'photos', 'records', 'code', 'blog', 'contact']
 
+configure do
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['CONSUMER_KEY']
+    config.consumer_secret     = ENV['CONSUMER_SECRET']
+    config.access_token        = ENV['ACCESS_TOKEN']
+    config.access_token_secret = ENV['ACCESS_SECRET']
+  end
+  set :twitter_client, client
+end
+
 get '/' do
   haml :index
 end
@@ -14,12 +24,14 @@ end
 
 CATEGORIES.each do |category|
   get "/latest_#{category}.json" do
+    content_type :json
     send("latest_#{category}").to_json
   end
 end
 
 helpers do
   def latest_tweets
-    [{content: "tweet"}]
+    tweets = settings.twitter_client.user_timeline("GordonDiggs", :exclude_replies => true, :count => DEFAULT_NUM_ITEMS)
+    tweets.map { |t| {text: t.text, favorites: t.favorite_count, retweets: t.retweet_count, uri: t.uri, created_at: t.created_at} }
   end
 end
