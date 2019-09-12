2ce10e6bb983adb703c08e4b4e8c75d3b397adb7
Gordon Diggs <gordon@gordondiggs.com>
2019-09-12 14:31:34 -0400

    Upgrade packages and add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,19 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
-    "gatsby-plugin-netlify"
+    "gatsby-plugin-netlify",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-20761002-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    }
   ],
   developMiddleware: app => {
     app.use(
